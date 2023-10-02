56d1117aa978582d5d73bb3d67a2e1f70af2e8a7
Ryan Spishock <22054759+rspishock@users.noreply.github.com>
2023-10-02 19:20:08 -0400

    Updated various files

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ##phishing.py
 Description: This script will parse .eml files extracting common points used during the analysis of potential phishing attacks.
 
-### Usage
+###Usage
 Script has the following permissions: 766
 The user will not need to call the Python3 interpreter to execute the script.
-./phishing.py -f <.eml file> 
+./phishing.py -i <.eml file> -o <OUTPUT FILE>
--- phishing.py
+++ phishing.py
@@ -12,18 +12,38 @@ from time import sleep
 def get_arguments():
     """Get user supplied arguements from terminal"""
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f', '--file', dest='file', help='The .eml file to examine.')
+    parser.add_argument('-i', '--input', dest='input', help='The .eml file to analyze.')
+    parser.add_argument('-o', '--output', dest='output', help='File to write parsed strings to.')
     # parser.add_argument('-h', '--help', )
     options = parser.parse_args()
 
     return options
 
+# argument variables
+options = get_arguments()
+input_file = options.input
+output_file = options.output
 
 print('\n\t\t\t\t***DISCLAIMER***')
-print('\nAll infomration returned from this script should be considered at face value and as a potential jump point for analysis.')
-print('\nMalware authors will often put code into their malware in an attempt to steer analysts away from the actual intent of their code.py.')
+print('\nAll information returned from this script should be considered at face value and as a potential jump point for further analysis.')
 print('\nUpon completion of the script, a deeper analysis should be performed to determine whether the suspected code is in fact malware.\n')
 # script will pause for 15 seconds to give the user a chance to read the disclaimer
 sleep(15)
 
 try:
+    if path.exists(input_file):
+        with open(input_file) as file_object:
+            for line in file_object:
+                #recipient regex
+                email_regex = re.compile(f'[a-z0-9]@[a-z0-9].[a-z]')
+                file = email_regex.search(line)
+                #subject regex
+                subject_regex = re.compile(f'Subject [a-z0-9]@[a-z0-9].[a-z]')
+                file = subject_regex.search(line)
+                #sender regex
+
+                #x-sender regex
+
+                #reply regex
+
+                #date/time regex
